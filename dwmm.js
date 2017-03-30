5641e8bb61ef1ce5822619f949da80101f8239d2
Tim Beadle <tim.beadle@lovehoney.co.uk>
2017-03-30 21:07:49 +0100

    Remove commented-out code

--- dwmm.js
+++ dwmm.js
@@ -25,39 +25,6 @@ document.addEventListener('DOMContentLoaded', function() {
 		console.log(data);
 	}
 
-
-	// const apiEndpoint = 'http://spreadsheet.glitch.me/?key=1c448_yKoiWrK6sf4cHVYv-KV29bzCOQO7EJlVDxDzgM';
-	//
-	// let header = new Headers({
-	// 	'Access-Control-Allow-Origin':'*'
-	// });
-	//
-	// let sentData={
-	// 	method: 'GET',
-	// 	mode: 'no-cors',
-	// 	header: header
-	// };
-	//
-	// fetch(apiEndpoint, sentData)
-	// 	.then(function(response) {
-	// 		if (!response.ok) {
-	// 			console.log(response.statusText);
-	// 			throw Error(response.statusText);
-	// 		}
-	// 		return response;
-	// 	}).then(function(response) {
-	// 		console.log(response);
-	// 		var tableOptions = {
-	// 			"data": response.data,
-	// 			"pagination": 10,
-	// 			"tableDiv": "#siteTable",
-	// 			"filterDiv": "#siteTableFilter",
-	// 			"templateID": "tableTemplate"
-	// 		}
-	// 		Sheetsee.makeTable(tableOptions)
-	// 		Sheetsee.initiateTableFilter(tableOptions)
-	//
-	// 	});
 	window.addEventListener('DOMContentLoaded', init);
 
 
